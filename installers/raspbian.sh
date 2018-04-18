3578f9e487b12547e6d15b56baee3ec57747d077
njkeng <njkotzur@hotmail.com>
2018-04-18 22:19:04 +1000

    Update name of main installation function

--- installers/raspbian.sh
+++ installers/raspbian.sh
@@ -1,6 +1,6 @@
 UPDATE_URL="https://raw.githubusercontent.com/njkeng/PiHelmetCam/master/"
-wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/raspapcommon.sh
-source /tmp/raspapcommon.sh && rm -f /tmp/raspapcommon.sh
+wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/pihelmetcamcommon.sh
+source /tmp/pihelmetcamcommon.sh && rm -f /tmp/pihelmetcamcommon.sh
 
 function update_system_packages() {
     install_log "Updating sources"
@@ -12,4 +12,4 @@ function install_dependencies() {
     sudo apt-get install lighttpd $php_package git hostapd dnsmasq || install_error "Unable to install dependencies"
 }
 
-install_raspap
+install_pihelmetcam
