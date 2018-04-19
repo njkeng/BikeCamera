ae4409a8f2f1ce14728f1008dbc0f9518edd5c64
njkeng <njkotzur@hotmail.com>
2018-04-19 12:48:25 +1000

    Fix importing of PiCamera

--- installers/video.py
+++ installers/video.py
@@ -2,7 +2,7 @@
 
 # Python script to record video
 #
-import picamera
+from picamera import PiCamera
 from gpiozero import Button, LED
 from threading import Timer
 from signal import pause
@@ -31,7 +31,7 @@ ledGPIO = 7             # LED is connected to GPIO 7 (pin 26)
 def output_files():
 
     datetime = datetime.now().isoformat()
-    filename = vid_dir 'vid%s.jpg' % datetime
+    filename = vid_dir + 'vid%s.jpg' % datetime
     print (filename)
     yield io.open(filename)
 
@@ -39,13 +39,13 @@ def output_files():
 #
 def when_pressed():
 
-    if ! camera.recording()
+    if ! camera.recording():
         print ("Starting recording")
         camera.start_recording(output_files(), quality=hc_quality, bitrate=hc_bitrate)
         led.on()
         split_timer.start()
 
-    if camera.recording()
+    if camera.recording():
         print ("Stopping recording")
         camera.stop_recording()
         led.off()
@@ -55,9 +55,22 @@ def when_pressed():
 #
 def video_split():
 
-    if camera.recording()
+    if camera.recording():
         camera.split_recording(output_files())
         split_timer.start()
+
+# Update video annotation with current time and date
+#
+def update_annotation():
+
+    if camera.recording():
+        camera.annotate_text = datetime.now().isoformat()
+        annotation_timer.start()
+
+        # Wait for a delay time
+        # The wait_recording function should be called so 
+        # that all camera errors are shown on screen
+        camera.wait_recording(0.2)
 
 
 camera = PiCamera()
@@ -73,6 +86,7 @@ button = Button(buttonGPIO)
 button.when_pressed = when_pressed
 
 split_timer = Timer(vid_length * 60, video_split)
+annotation_timer = Timer(1, update_annotation)
 
 print ("Waiting for a button press")
 
