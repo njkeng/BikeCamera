3d40da053f028b814b37c7dce1abb7229f910af9
njkeng <njkotzur@hotmail.com>
2018-04-19 19:31:10 +1000

    Revert "Fix function enable_camera"
    This reverts commit 0b89bf80c5a5d0d4058617b94aa5e14aadf72f28.

--- installers/common.sh
+++ installers/common.sh
@@ -371,14 +371,11 @@ function patch_system_files() {
 }
 
 function enable_camera() {
-    sed $CONFIG -i -e "s/^startx/#startx/"
-    sed $CONFIG -i -e "s/^fixup_file/#fixup_file/"
-    set_config_var start_x 1 $CONFIG
-    CUR_GPU_MEM=$(get_config_var gpu_mem $CONFIG)
-    if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
-      set_config_var gpu_mem 128 $CONFIG
-    fi
-}
+    [ -e /boot/config.txt ] || touch /boot/config.txt
+    set_config_var start_x 1 /boot/config.txt
+    set_config_var gpu_mem 128 /boot/config.txt
+    sed /boot/config.txt -i -e "s/^startx/#startx/"
+    sed /boot/config.txt -i -e "s/^fixup_file/#fixup_file/"
 }
 
 function install_complete() {
