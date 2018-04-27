6becc60e81fa4e54862477b25fb879aa6663eecd
njkeng <njkotzur@hotmail.com>
2018-04-27 22:13:41 +1000

    Check for ffmpeg already running

--- installers/process_vid.sh
+++ installers/process_vid.sh
@@ -8,10 +8,18 @@ function process_video_files() {
 	ffmpeg_input_dir=$(sudo cat /etc/pihelmetcam/video/video.ini | grep --only-matching --perl-regexp "(?<=ffmpeg_input_dir = \")\S+(?=\")")
 
 	# Numeric variables
-    cull_free_space=$(sudo cat /etc/pihelmetcam/video/video.ini | grep --only-matching --perl-regexp "(?<=cull_free_space = ).+")
+    cull_free_space=$(sudo cat /etc/pihelmetcam/video/video.ini | grep --only-matching --perl-regexp "(?<=cull_free_space = )\w+")
 
     # Process all of the video files in the 'processing' folder
     while : ; do
+
+    	# Check for ffmpeg already running
+    	ffmpeg_count=$(ps ax | grep -c ffmpeg)
+    	if [ $ffmpeg_count -gt 1 ]; then
+    		echo "ffmpeg already running"
+    		break
+    	fi
+
     	# Get the file name of the oldest video file in the folder
 		input_file=$(find $ffmpeg_input_dir -name '*.h264' -printf '%P\n' | sort | head -1)
 
@@ -20,6 +28,7 @@ function process_video_files() {
 
 		# Check for all files completed
 	    if [ "$input_file" == "" ]; then
+	    	echo "Found no files to process"
 	    	break
 	    fi
 
@@ -62,7 +71,7 @@ function process_video_files() {
 		fi
 
     	# Get the file name of the oldest video file in the folder
-		oldest_file=$(find $ffmpeg_output_dir -name '*mp4' -printf '%P\n' | sort | head -1)
+		oldest_file=$(find $ffmpeg_output_dir -name 'vid*' -printf '%P\n' | sort | head -1)
 
 		# Check if there are no more files to delete
 		if [ "$oldest_file" == "" ]; then
